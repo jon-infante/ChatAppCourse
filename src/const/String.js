743b4bae723535e292ece382bf3537666d8dfb7a
joninfante <jonjoninfante@gmail.com>
2021-08-06 13:12:09 -0400

    Signing name, button, and email field

---
+++ src/const/String.js
@@ -0,0 +1,9 @@
+const Strings = {
+    Join: 'Join Now',
+    InvalidEmailAddress: 'Invalid Email Address!',
+    PasswordFieldEmpty: 'Password Field cannot be empty!',
+    EmailPlaceHolder: 'Email Address',
+    PasswordPlaceholder: 'Password'
+}
+
+export default Strings
---
+++ src/utils/Utility.js
@@ -0,0 +1,13 @@
+export default class Utility {
+
+    static isValidField = (term) => {
+        let length = term.trim().length
+        return length > 0 ? true : false
+    }
+
+    static isEmailValid = (term) => {
+        const expression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let isValid = expression.test(String(term).toLowerCase())
+        return
+    }
+}
